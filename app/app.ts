f9d52a986c3ed3fbe7060ec8d4edf2c22d2563a2
Gáspár Bence <bence.gaspar@sightspot.hu>
2020-12-15 13:40:58 +0100

    set up mysql connection.

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,25 @@
-// lib/app.ts
 import express = require('express');
 
 // Create a new express application instance
 const app: express.Application = express();
+
+// Connect to MySQL database
+let mysql = require('mysql');
+
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'ecommerce_db'
+})
+
+connection.connect((err: any) => {
+   if (err) {
+       return console.error('error: ' + err.message);
+   }
+});
+
+console.log('Connected to the MySQL server.');
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
--- build/app.js
+++ build/app.js
@@ -1,12 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// lib/app.ts
 var express = require("express");
 // Create a new express application instance
 var app = express();
+// Connect to MySQL database
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'ecommerce_db'
+});
+connection.connect(function (err) {
+    if (err) {
+        return console.error('error: ' + err.message);
+    }
+});
+console.log('Connected to the MySQL server.');
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
+// Compile application:
+// npm run tsc
+// Running the express:
+// node build/app.js
